384c4b1c24dbce7f6b12fc970c44c8fe3c0de97f
lazydevcoder <lazydevcoder@gmail.com>
2023-03-23 13:41:13 +0530

    fixed Engines

--- data/database.js
+++ data/database.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 
 export const connectDB = async () => {
-    mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-    console.log("Connected to MongoDB");
+    mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then((c) => {
+    console.log(`Database Connected with ${c.connection.host} `);
     }).catch((err) => {
     console.log(err);
     });
